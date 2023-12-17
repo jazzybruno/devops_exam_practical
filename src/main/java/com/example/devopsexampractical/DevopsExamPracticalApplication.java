4ff175847bd21ddd530c5593f04981d688e7baab
jazzybruno <95282931+jazzybruno@users.noreply.github.com>
2023-12-17 23:25:13 +0200

    [Add] Added the initialisation

---
+++ src/main/java/com/example/devopsexampractical/DevopsExamPracticalApplication.java
@@ -0,0 +1,13 @@
+package com.example.devopsexampractical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsExamPracticalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevopsExamPracticalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/devopsexampractical/DevopsExamPracticalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devopsexampractical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsExamPracticalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
