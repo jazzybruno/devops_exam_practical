0ceac45dc0f0fff9425a642e220db833459b6483
jazzybruno <95282931+jazzybruno@users.noreply.github.com>
2023-12-18 10:23:01 +0200

    [Add] Initialising the End to End Tests

---
+++ src/test/java/com/example/devopsexampractical/v1/controllers/MathsControllerEndToEndTest.java
@@ -0,0 +1,12 @@
+package com.example.devopsexampractical.v1.controllers;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class MathsControllerEndToEndTest {
+
+}
