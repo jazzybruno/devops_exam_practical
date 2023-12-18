c957a11bfc4e2942da86fc04d1114e49314482d0
jazzybruno <95282931+jazzybruno@users.noreply.github.com>
2023-12-18 09:04:07 +0200

    [Add] Added the mathe operation implementation logic

--- src/main/java/com/example/devopsexampractical/v1/serviceImpl/MathOperatorImpl.java
+++ src/main/java/com/example/devopsexampractical/v1/serviceImpl/MathOperatorImpl.java
@@ -1,4 +1,26 @@
 package com.example.devopsexampractical.v1.serviceImpl;
 
-public class MathOperatorImpl {
+import com.example.devopsexampractical.v1.exceptions.InvalidOperationException;
+import com.example.devopsexampractical.v1.services.MathOperator;
+
+public class MathOperatorImpl implements MathOperator {
+    @Override
+    public double doMath(double operand1, double operand2, String
+            operation) throws InvalidOperationException{
+        if ("/".equals(operation) && operand2 == (double) 0) {
+            throw new InvalidOperationException("Cannot divide by 0");
+        }
+        switch (operation) {
+            case "*":
+                return operand1 * operand2;
+            case "/":
+                return operand1 / operand2;
+            case "+":
+                return operand1 + operand2;
+            case "-":
+                return operand1 - operand2;
+            default:
+                throw new RuntimeException("Unknown operation");
+        }
+    }
 }
