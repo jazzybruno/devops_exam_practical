16aeaa3219a68e6ac9d628842be78217f6cfea71
jazzybruno <95282931+jazzybruno@users.noreply.github.com>
2023-12-18 09:45:37 +0200

    [Add] Initiaiting the intergration testing

---
+++ src/test/java/com/example/devopsexampractical/v1/controllers/MathsControllerIntegrationTest.java
@@ -0,0 +1,10 @@
+package com.example.devopsexampractical.v1.controllers;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@AutoConfigureWebMvc
+class MathsControllerIntegrationTest {
+
+}
