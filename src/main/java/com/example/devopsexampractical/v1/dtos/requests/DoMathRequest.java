9f6969cf8cf4fcd729df8e6406fbe101a3b313ae
jazzybruno <95282931+jazzybruno@users.noreply.github.com>
2023-12-18 09:05:43 +0200

    [Add] Added the Request and the Response DTOs

--- src/main/java/com/example/devopsexampractical/v1/dtos/requests/DoMathRequest.java
+++ src/main/java/com/example/devopsexampractical/v1/dtos/requests/DoMathRequest.java
@@ -1,4 +1,16 @@
 package com.example.devopsexampractical.v1.dtos.requests;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class DoMathRequest {
+    private double operand1;
+    private double operand2;
+    private String operation;
 }
--- src/main/java/com/example/devopsexampractical/v1/dtos/responses/CalcResponse.java
+++ src/main/java/com/example/devopsexampractical/v1/dtos/responses/CalcResponse.java
@@ -1,4 +1,14 @@
 package com.example.devopsexampractical.v1.dtos.responses;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class CalcResponse {
+    double calcResponse;
 }
