8cb3458fa7e82c2bb2b3c31a4983e841e2dc3b9b
jazzybruno <95282931+jazzybruno@users.noreply.github.com>
2023-12-18 09:26:39 +0200

    [Add] Tested the application and it is running smoothly

--- src/main/java/com/example/devopsexampractical/v1/controllers/MathsController.java
+++ src/main/java/com/example/devopsexampractical/v1/controllers/MathsController.java
@@ -9,9 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value = "/api/calc")
 public class MathsController {
 
     private final MathOperator mathOperator;
@@ -21,7 +23,7 @@ public class MathsController {
         this.mathOperator = mathOperator;
     }
 
-    @PostMapping("/maths")
+    @PostMapping("/do-maths")
     public ResponseEntity<CalcResponse> doMath(@RequestBody DoMathRequest doMathRequest) {
         try {
             Double result = mathOperator.doMath(doMathRequest.getOperand1(), doMathRequest.getOperand2(), doMathRequest.getOperation());
